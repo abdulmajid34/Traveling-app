0f3c842131e6468be31dfff0d2c4c43f0f79d2ad
abdulmajid <majid@kebun.tech>
2022-09-19 09:14:09 +0700

    add routes

--- traveling_app/lib/main.dart
+++ traveling_app/lib/main.dart
@@ -9,10 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: SplashPage(),
-      home: GetStartedPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStartedPage(),
+      },
+      // home: GetStartedPage(),
     );
   }
 }
